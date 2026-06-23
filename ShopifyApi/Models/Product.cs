e98f2c606d02796e080d0e86b67287859ecbd9e1
bellanzo <abimbolaomisakin678@gmail.com>
2026-06-23 03:44:38 +0100

    Add project files.

---
+++ ShopifyApi/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopifyApi.Models;
+
+public class Product
+{
+    public int Id { get; set; } // Primary key 
+    public string Name { get; set; } = string.Empty; // string.empty prevents null warning
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
